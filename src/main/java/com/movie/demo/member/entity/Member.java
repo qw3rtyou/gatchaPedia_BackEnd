f9921dc8736db22114c4c602c8f360dee2345e0a
RYUL99 <seunglul1999@gmail.com>
2024-04-25 18:12:37 +0900

    Member 엔티티 매핑

---
+++ src/main/java/com/movie/demo/member/entity/Member.java
@@ -0,0 +1,31 @@
+package com.movie.demo.member.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Member {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+}
